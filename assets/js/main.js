d1d9dc1f74397a330b3be43a46835e01223564f7
SC2003 <46285019+SC2003@users.noreply.github.com>
2022-11-01 19:58:18 +0530

    removed unused code

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,35 +1,37 @@
 // Rock-Paper-Scissors-Game
 const validChoices = ["rock", "paper", "scissors"];
-const totalMoves = [1,2,3,4,5];
+
 const playerScoreElement = document.querySelector(".player-score");
 
 const computerScoreElement = document.querySelector(".computer-score");
 
 const winnerElement = document.querySelector(".winner");
 
-let outputArea = document.querySelector("#output");
-let output;
+const outputArea = document.querySelector("#output");
+
 const choiceBtn = document.querySelectorAll(".choices-btn-container");
+
 let playerScore = parseInt(0);
 
 let computerScore = parseInt(0);
 
+let tie = parseInt(0);
+
 const getComputerChoice = () => {
     const randomChoice = Math.floor(Math.random() * validChoices.length);
-    return validChoices[randomChoice]
-    // const randomChoice = Math.floor(Math.random() * validChoices.length) + 1;
-    // return validChoices[randomChoice - 1]
-
+    return validChoices[randomChoice];
 }
+
 const getPlayerChoice = (e) => {
     playRound(e.target.id, getComputerChoice());
 }
-const playRound = (playerSelection, computerSelection) => {
 
+const playRound = (playerSelection, computerSelection) => {
     updateScore();
-    winner();
+    stopRound();
     if (computerSelection === "rock") {
         if (playerSelection === "rock") {
+            tie++;
             outputArea.textContent = (`It's a Tie!!
             \nComputer's Choice: "${computerSelection}"
             \nPlayer's Choice: "${playerSelection}"`);
@@ -50,6 +52,7 @@ const playRound = (playerSelection, computerSelection) => {
         }
     } else if (computerSelection === "paper") {
         if (playerSelection === "paper") {
+            tie++;
             outputArea.textContent = (`It's a Tie!!
             \nComputer's Choice: "${computerSelection}"
             \nPlayer's Choice: "${playerSelection}"`);
@@ -70,6 +73,7 @@ const playRound = (playerSelection, computerSelection) => {
         }
     } else if (computerSelection === "scissors") {
         if (playerSelection === "scissors") {
+            tie++;
             outputArea.textContent = (`It's a Tie!!
             \nComputer's Choice: "${computerSelection}"
             \nPlayer's Choice: "${playerSelection}"`);
@@ -94,54 +98,17 @@ const playRound = (playerSelection, computerSelection) => {
         \nPlayer's   Choice: "${playerSelection}"`);
     }
 };
+
 const updateScore = () => {
     playerScoreElement.textContent = (`Player's Score: ${playerScore}`);
-
     computerScoreElement.textContent = (`Computer's Score: ${computerScore}`);
 };
 
-const game = (e) => {
-    const playerSelection = e.target.id;
-    const computerSelection = getComputerChoice();
-    // totalMoves.forEach(i=>{
-    //     const playerSelection = e.target.id;
-    //     const computerSelection = getComputerChoice();
-    //     if (i === totalMoves[4]);
-    //     playRound(playerSelection, computerSelection);
-        
-    // })
-    let i =0;
-    while(i<5){
-        i++;
-        playRound(playerSelection, computerSelection);
-        break;
-    }
-}
 choiceBtn.forEach((choice => {
-    choice.addEventListener('click', game, false)
+    choice.addEventListener('click', getPlayerChoice)
 }))
-// choiceBtn.forEach((choice => {
-//     choice.addEventListener('click', getPlayerChoice)
-// }))
 
-
-// choiceBtn.forEach((choice => {
-//     choice.addEventListener('click', (e) => {
-//         // let playerSelection1 = e.target.id;
-//         // let btnEvent = choice;
-//         // const computerSelection = getComputerChoice();
-//         // // console.log(playerSelection)
-//         // // console.log(computerSelection)
-//         // // ,{once: true}
-//         // playRound(playerSelection, computerSelection);
-
-//         playRound(e.target.id, getComputerChoice());
-
-//         // game(playerSelection1, btnEvent);
-
-//     })
-// }))
-const winner = (playerScore, computerScore) => {
+const winner = () => {
     if (playerScore > computerScore) {
         return `Player Won by Total Score of ${playerScore}`;
     } else if (playerScore < computerScore) {
@@ -150,8 +117,17 @@ const winner = (playerScore, computerScore) => {
         return `Tie with The Player Scoring: ${playerScore} and The Computer Scoring: ${computerScore}`;
     }
 }
-const restart = () => location.replace(location.href.split('#')[0]);
 
-winnerElement.textContent = winner(playerScore, computerScore);
-// outputArea.textContent = output;
-// outputArea.textContent = winner(playerScore, computerScore);
+const stopRound = () => {
+    if (playerScore + computerScore + tie === 5) {
+        setTimeout(() => {
+            outputArea.textContent = `Round Ended!! \nClick Restart to Play Again`;
+        }, 500);
+        Array.prototype.forEach.call(choiceBtn, (e) => {
+            e.removeEventListener("click", getPlayerChoice);
+        });
+        winnerElement.textContent = winner();
+    }
+    return false;
+}
+const restart = () => location.replace(location.href.split('#')[0]);
